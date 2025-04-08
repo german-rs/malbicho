75162a5943ef08a00992a45e8b3a29403d5d52c3
german-rs <ing.germanriveros@gmail.com>
2025-04-08 19:09:01 -0400

    Experimento r0002 en lenguaje C

---
+++ ransomware/r0002/r0002.c
@@ -0,0 +1,12 @@
+/**
+ * @file r0002.c
+ * @brief Encripta archivos individuales o todos los archivos en un directorio usando AES-256-CBC
+ *
+ * Compilación: clang -o r0002 r0002.c -lssl -lcrypto
+ * Ejecución: ./r0002 <ruta_al_directorio_o_archivo>
+ *
+ * @author
+ * Germán Riveros S.
+ * @date 06/04/2025
+ * @version 1.1
+ */
